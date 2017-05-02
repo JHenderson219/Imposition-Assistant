8dd362c85a4f3c999f8016e45d4ca7e47892bdb0
Jordan Henderson <hendersonj219@gmail.com>
2017-05-02 16:13:58 -0700

    fix errors and typos; add console log statemtents to impose function

--- scripts/main.js
+++ scripts/main.js
@@ -22,13 +22,13 @@ $("#calculate").on("click", function (){
 });
 */
 
-var input = "100\n100\n250\n100\n500\n100";
+var input = "240\n480\n240\n240\n240\n360";
 var up;
 var duplex;
 
-var arr = input.split("\n"); //Splits input string on newline, assigns to array.
+var inputArr = input.split("\n"); //Splits input string on newline, assigns to array.
 
-function page(index, qty){
+function Page(index, qty){
 	this.index = index;
 	this.qty = parseInt(qty,10);
 }
@@ -44,15 +44,15 @@ function populatePages(arr){
 }
 
 function impose(pageArr, up, duplex, arr){
-	let len = pageArr.length
+	let len = pageArr.length;
 	let sq = 0;
-	let qsq = 0;
+	let qsq = 1;
 	let factorIndex = 0;
-
+	let shuffleRules = "";
 	sq = pageArr[0].qty;
-	
 	for(let i = 1; i < len;i++){
-		pageArr[i].qty < sq ? sq = pageArr[i].qty : sq = sq ;				//Find smallest quantity.
+		if (pageArr[i].qty < sq)
+		sq = pageArr[i].qty;				//Find smallest quantity.
 	}
 	
 	for(let i = 1; i < len;i++){
@@ -61,12 +61,15 @@ function impose(pageArr, up, duplex, arr){
 	}
 
 	factors = factorize(up); 									//Assign factors of 'up' as a string to factors.
-	
+	console.log("qsq is: "+qsq);
+	console.log("sq is: "+sq);
+	console.log ("factors array is: "+factors);
+	console.log ("up over qsq is: "+(up/qsq));
 	for(let i = 0; i < factors.length; i++){ 
 		if(factors[i] == (up/qsq)) 								//Find factor in factor array.
 			factorIndex = i;
 	}
-
+	console.log("factor index is: "+factorIndex);
 	if(factorIndex = 0){										//If up is not a factor
 		//Go do something else
 	}
@@ -74,9 +77,6 @@ function impose(pageArr, up, duplex, arr){
 	else{
 
 	}
-			
-	
-
 
 	return shuffleRules;
 }
@@ -85,7 +85,6 @@ function factorize(num){
 	let factors = [0];
 	let j = 1;
 	for(let i = 0;i < num;i++){
-		console.log(24%4);
 		if(num%i == 0){
 			factors[j] = i;
 			j++;
@@ -93,3 +92,7 @@ function factorize(num){
 	}
 	return factors;
 }
+
+console.log (impose(populatePages(inputArr), 24, false, [] ));
+
+console.log(240%12);
